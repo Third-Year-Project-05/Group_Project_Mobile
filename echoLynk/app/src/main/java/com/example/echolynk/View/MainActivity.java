c214ccedd43e3ab55ef6b945fd746c4d988f0c82
TharinduDakshina <tharindudakshina527@gmail.com>
2024-06-13 00:11:51 +0530

    crate branch and add mvvm Architectur

--- echoLynk/app/src/main/java/com/example/echolynk/View/MainActivity.java
+++ echoLynk/app/src/main/java/com/example/echolynk/View/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.echolynk;
+package com.example.echolynk.View;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.echolynk.R;
 
 public class MainActivity extends AppCompatActivity {
 
