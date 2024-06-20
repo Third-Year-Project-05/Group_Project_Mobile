5bc4da5d75f7cc257a64f3b8003346bec364745f
TharinduDakshina <tharindudakshina527@gmail.com>
2024-06-20 10:19:11 +0530

    update verfy page

--- echoLynk/app/src/main/java/com/example/echolynk/View/MainActivity.java
+++ echoLynk/app/src/main/java/com/example/echolynk/View/MainActivity.java
@@ -18,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_create_account);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signUp), (v, insets) -> {
+        setContentView(R.layout.activity_verify_code);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.verifyCode), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
