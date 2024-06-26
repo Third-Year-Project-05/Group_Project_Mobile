db006e6f891a6300ce4fd3de14ad0b250af7e652
Sandaru Samarasekara <sandarusamarasekara@gmail.com>
2024-06-26 23:57:27 +0530

    profile UI design

--- echoLynk/app/src/main/java/com/example/echolynk/View/MainActivity.java
+++ echoLynk/app/src/main/java/com/example/echolynk/View/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -35,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
 
-    RelativeLayout mainLayout;
+    RelativeLayout mainLayout, mainLayoutHeader;
     TextView userName;
     de.hdodenhof.circleimageview.CircleImageView userImage;
 
@@ -57,6 +58,10 @@ public class MainActivity extends AppCompatActivity {
         mainLayout = findViewById(R.id.main);
         userName = findViewById(R.id.user_name);
         userImage = findViewById(R.id.user_image);
+        mainLayoutHeader = findViewById(R.id.main_header);
+
+        replaceFragment(new HomeFragment());
+        setHeader(true);
 
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
@@ -64,21 +69,28 @@ public class MainActivity extends AppCompatActivity {
             if (item.getItemId() == R.id.home_frame){
                 replaceFragment(new HomeFragment());
                 setHeaderColours(0);
+                setHeader(true);
             } else if (item.getItemId() == R.id.call_frame) {
                 replaceFragment(new CallFragment());
                 mainLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.white));
                 setHeaderColours(1);
+                setHeader(true);
 
             } else if (item.getItemId() == R.id.speech_frame) {
                 replaceFragment(new SpeechFragment());
                 setHeaderColours(1);
+                setHeader(true);
             } else if (item.getItemId() == R.id.games_frame) {
                 replaceFragment(new GamesFragment());
                 setHeaderColours(0);
+                setHeader(true);
             } else if (item.getItemId() == R.id.profile_frame) {
                 replaceFragment(new ProfileFragment());
+                mainLayoutHeader.setVisibility(View.GONE);
+                setHeader(false);
             } else {
                 replaceFragment(new TestFragment());
+                setHeader(true);
             }
             return true;
         });
@@ -99,6 +111,15 @@ public class MainActivity extends AppCompatActivity {
             binding.bottomNavigationView.setSelectedItemId(R.id.home_frame);
             replaceFragment(new HomeFragment());
 
+    }
+
+    public void setHeader(boolean show){
+        if (show){
+            mainLayoutHeader.setVisibility(View.VISIBLE);
+        }
+        else{
+            mainLayoutHeader.setVisibility(View.GONE);
+        }
     }
 
     private void setHeaderColours(int backColour){
