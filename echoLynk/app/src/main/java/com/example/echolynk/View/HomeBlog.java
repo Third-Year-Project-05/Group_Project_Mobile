9059c74531fa57499bb603c837cbdb3e603c178a
TharinduDakshina <tharindudakshina527@gmail.com>
2024-06-29 23:41:44 +0530

    Blog compleate

--- echoLynk/app/src/main/java/com/example/echolynk/View/HomeBlog.java
+++ echoLynk/app/src/main/java/com/example/echolynk/View/HomeBlog.java
@@ -20,6 +20,7 @@ import com.example.echolynk.Model.Blog;
 import com.example.echolynk.R;
 import com.example.echolynk.Utils.OnBlogClickListener;
 import com.example.echolynk.View.Adapter.BlogAdapter;
+import com.example.echolynk.View.Adapter.EmptyAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +37,7 @@ public class HomeBlog extends AppCompatActivity implements OnBlogClickListener {
         recyclerView=findViewById(R.id.blog_recycle_view);
 
         List<Blog> blogs=new ArrayList<>();
+
         blogs.add(new Blog("What is Text-to-Speech?","by Echolynk",R.drawable.dummy_blog_img1,"Imagine if your books could talk! With text-to-speech (TTS) technology, they can. From War and Peace to DMs, the machine can read it all out loud. And it doesn’t even need to sound like a robot. Once just a sci-fi dream, TTS now brings words to life — with the voice of your choice\n" +
                 "\u200D\n" +
                 "For those supporting Deaf and hard-of-hearing individuals, in education or at work, TTS offers a bridge to conversations with hearing peers by transforming text into spoken conversation that's more engaging and inclusive than ever."));
@@ -55,9 +57,14 @@ public class HomeBlog extends AppCompatActivity implements OnBlogClickListener {
         blogs.add(new Blog("What is Text-to-Speech?","by Echolynk",R.drawable.dummy_blog_img1,"Read More >"));
 
 
-        if (recyclerView != null){
-            recyclerView.setLayoutManager(new LinearLayoutManager(this));
-            recyclerView.setAdapter(new BlogAdapter(getApplicationContext(),blogs,this));
+        if (recyclerView != null || !blogs.isEmpty()){
+            if (blogs.isEmpty()){
+                recyclerView.setLayoutManager(new LinearLayoutManager(this));
+                recyclerView.setAdapter(new EmptyAdapter(getApplicationContext(),R.drawable.empty_icon));
+            }else {
+                recyclerView.setLayoutManager(new LinearLayoutManager(this));
+                recyclerView.setAdapter(new BlogAdapter(getApplicationContext(),blogs,this));
+            }
         }else {
             Log.d("recyclerView check", "recyclerView null ");
             //check the load fragment
