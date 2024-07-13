857c0610f2bb374a8dbe3d3eb034893389ba27d6
TharinduDakshina <tharindudakshina527@gmail.com>
2024-07-13 19:31:54 +0530

    speech to text component is init and catch the speech then show the speech massage box

--- echoLynk/app/src/main/java/com/example/echolynk/View/LiveConversationChat.java
+++ echoLynk/app/src/main/java/com/example/echolynk/View/LiveConversationChat.java
@@ -2,27 +2,31 @@ package com.example.echolynk.View;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.AudioRecord;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
-import android.util.Log;
+import android.speech.SpeechRecognizer;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
-
 
 
 import com.example.echolynk.R;
@@ -31,12 +35,10 @@ import com.example.echolynk.Utils.onClickListener;
 import com.example.echolynk.View.Adapter.AnswerAdapter;
 import com.example.echolynk.View.Adapter.LiveUserAdapter;
 import com.example.echolynk.View.Adapter.SenderAdapter;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class LiveConversationChat extends AppCompatActivity implements onClickListener {
 
@@ -46,12 +48,17 @@ public class LiveConversationChat extends AppCompatActivity implements onClickLi
     RecyclerView chatRecycleView;
     RecyclerView suggestion_answer;
     private EditText massageBox;
+    private SpeechRecognizer speechRecognizer;
     private ImageButton keyboardBtn;
     private ImageButton mikeBtn;
     private ImageButton pauseBtn;
     private ImageButton closeBtn;
+    private boolean isRecording = false;
+    AlertDialog.Builder alertSpeechDialog;
+    AlertDialog alertDialog;
 
-    private static final int REQUEST_AUDIO_PERMISSION_CODE=1;
+
+    private static final int RecodeAudioRequestCode = 1;
 
 
     @SuppressLint("MissingInflatedId")
@@ -62,16 +69,15 @@ public class LiveConversationChat extends AppCompatActivity implements onClickLi
         setContentView(R.layout.activity_live_conversation_chat);
 
 
-        relativeLayout=findViewById(R.id.live_conversation_chat);
+        relativeLayout = findViewById(R.id.live_conversation_chat);
         recyclerView = findViewById(R.id.live_users);
         chatRecycleView = findViewById(R.id.chat_view);
-        suggestion_answer=findViewById(R.id.suggestion_answers);
-        keyboardBtn=findViewById(R.id.keyboard_icon);
-        mikeBtn=findViewById(R.id.speech_icon);
-        pauseBtn=findViewById(R.id.pause_icon);
-        closeBtn=findViewById(R.id.close_icon);
-        massageBox=findViewById(R.id.write_massage);
-
+        suggestion_answer = findViewById(R.id.suggestion_answers);
+        keyboardBtn = findViewById(R.id.keyboard_icon);
+        mikeBtn = findViewById(R.id.speech_icon);
+        pauseBtn = findViewById(R.id.pause_icon);
+        closeBtn = findViewById(R.id.close_icon);
+        massageBox = findViewById(R.id.write_massage);
 
 
         List<Integer> live_users = new ArrayList<>();
@@ -79,31 +85,18 @@ public class LiveConversationChat extends AppCompatActivity implements onClickLi
         List<String> suggestions = new ArrayList<>();
 
 
-
         // set suggestions
-        setSuggestions(suggestions,suggestion_answer);
-
+        setSuggestions(suggestions, suggestion_answer);
 
 
         // set data for live users icon tab
-        setLiveUsers(live_users,recyclerView);
-
+        setLiveUsers(live_users, recyclerView);
 
 
         // set data for live chat view
-        setLiveChat(chatRecycleView,massageList);
+        setLiveChat(chatRecycleView, massageList);
 
-
-        // check the audio recoding permission
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_AUDIO_PERMISSION_CODE);
-        }
-
-        mikeBtn.setOnClickListener(view -> {
-            promptSpeechInput();
-        });
-
-
+        // click keyboard btn
         keyboardBtn.setOnClickListener(view -> {
             massageBox.requestFocus();
             InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -113,47 +106,106 @@ public class LiveConversationChat extends AppCompatActivity implements onClickLi
         });
 
 
+        // check the audio recoding permission
+        if (ContextCompat.checkSelfPermission(LiveConversationChat.this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            checkPermission();
+        }
+
+
+        speechRecognizer=SpeechRecognizer.createSpeechRecognizer(this);
+        final Intent speechIntent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+        speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                ViewGroup viewGroup=findViewById(android.R.id.content);
+                View dialogView= LayoutInflater.from(LiveConversationChat.this).inflate(R.layout.alert_custom,viewGroup,false);
+
+                alertSpeechDialog=new AlertDialog.Builder(LiveConversationChat.this);
+                alertSpeechDialog.setMessage("Listening....");
+                alertSpeechDialog.setView(dialogView);
+                alertDialog=alertSpeechDialog.create();
+                alertDialog.show();
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+              //  imageButton.setImageResource(R.drawable.mic);
+                ArrayList<String> arrayList=bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                massageBox.setText(arrayList.get(0));
+                alertDialog.dismiss();
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+
+
+        //click the mike btn
+        mikeBtn.setOnClickListener(view -> {
+            /*if (isRecording) {
+                //stopRecoding();
+            } else {
+                isRecording = true;
+
+            }*/
+
+            speechRecognizer.startListening(speechIntent);
+        });
+
+        closeBtn.setOnClickListener(view -> {
+            speechRecognizer.stopListening();
+        });
+
+
     }
 
-    private void promptSpeechInput() {
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-
-        try {
-            startActivityForResult(intent, 100);
-        } catch (ActivityNotFoundException a) {
-            Toast.makeText(getApplicationContext(), "Speech not supported on this device", Toast.LENGTH_SHORT).show();
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},RecodeAudioRequestCode);
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode) {
-            case 100:
-                if (resultCode == RESULT_OK && data != null) {
-                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    String spokenText = result.get(0);
-                    // Do something with the spoken text
-                    Toast.makeText(this, spokenText, Toast.LENGTH_LONG).show();
-                }
-                break;
-        }
-    }
-
-    private void setSuggestions(List<String> suggestions, RecyclerView suggestionAnswer) {
-        suggestions.add("Yes");
-        suggestions.add("No");
-        if (suggestionAnswer!=null) {
-            suggestionAnswer.setAdapter(new AnswerAdapter(getApplicationContext(),suggestions,this));
-        }
-    }
 
 
 
-    private void setLiveChat(RecyclerView chatRecycleView, List<String> massageList) {
+    private void setLiveChat (RecyclerView chatRecycleView, List < String > massageList){
         massageList.add("abc");
         massageList.add("10");
         massageList.add("safafa");
@@ -166,7 +218,7 @@ public class LiveConversationChat extends AppCompatActivity implements onClickLi
         }
     }
 
-    private void setLiveUsers(List<Integer> live_users, RecyclerView recyclerView) {
+    private void setLiveUsers (List < Integer > live_users, RecyclerView recyclerView){
         live_users.add(R.drawable.dummy_user_image);
         live_users.add(R.drawable.dummyprofileimage);
         live_users.add(R.drawable.dummy_blog_img2);
@@ -180,9 +232,35 @@ public class LiveConversationChat extends AppCompatActivity implements onClickLi
         }
     }
 
-    @Override
-    public void onClick(int position, View view) {
-        //set up on click function using live users
+    private void setSuggestions (List < String > suggestions, RecyclerView suggestionAnswer){
+        suggestions.add("Yes");
+        suggestions.add("No");
+        suggestions.add("No");
+        if (suggestionAnswer != null) {
+            suggestionAnswer.setAdapter(new AnswerAdapter(getApplicationContext(), suggestions, this));
+        }
     }
 
+    @Override
+    public void onRequestPermissionsResult ( int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == RecodeAudioRequestCode) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted
+            } else {
+                // Permission denied
+                Toast.makeText(this, "Audio Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onClick(int position, View view) {
+
+    }
 }
+
+
+
+
+
